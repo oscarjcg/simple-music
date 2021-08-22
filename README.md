a4fc120e4efe12f239d4aed3c1ed9cdb31de7fe0
Oscar Catari Gutierrez <oscarcatari@outlook.es>
2021-08-22 18:50:47 +0100

    [FIX] Android api 21. Internet connection

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Simple Music
 Just a simple Android app to search artist and explore music.
 
+# Develop Android API
+* API 26 (Android 8)
+
 # Features
-* Search bar and search result list with musical artists
+* Search bar for musical artists
 * Explore albums
 * Explore songs
+* Explore music videos
+* Play music videos, songs and share them
--- app/src/main/java/com/example/simplemusic/utils/Connectivity.kt
+++ app/src/main/java/com/example/simplemusic/utils/Connectivity.kt
@@ -1,12 +1,14 @@
 package com.example.simplemusic.utils
 
 import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
 import android.net.NetworkInfo
 import android.os.Build
 import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
 
 /**
  * Check if Internet is available.
@@ -22,19 +24,27 @@ class Connectivity {
                         connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
                     if (capabilities != null) {
                         if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                            Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                            //Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
                             return true
                         } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                            Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                            //Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
                             return true
                         } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
-                            Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                            //Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
                             return true
                         }
                     }
                 }
             }
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+                return internetCheck(context)
             return false
+        }
+
+        private fun internetCheck(c: Context): Boolean {
+            val cmg = c.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            return cmg.activeNetworkInfo?.isConnectedOrConnecting == true
         }
     }
 }
